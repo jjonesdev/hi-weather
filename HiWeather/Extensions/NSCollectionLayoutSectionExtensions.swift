cd87055284813227c3bedc31279469931dc5a1fa
Jordan Jones <jonesandcode@gmail.com>
2021-03-21 13:51:48 -0400

    Created helper and extension files
    - Created a CV LayoutSection helper to reduce boiler plate
    - Created Spacing constants

---
+++ HiWeather/Extensions/NSCollectionLayoutSectionExtensions.swift
@@ -0,0 +1,53 @@
+//
+//  NSCollectionLayoutSectionExtensions.swift
+//  HiWeather
+//
+//  Created by Jordan Jones on 3/21/21.
+//
+
+import UIKit
+
+extension NSCollectionLayoutSection {
+    enum Height {
+        case estimated(CGFloat)
+        case absolute(CGFloat)
+        case fractional(CGFloat)
+
+        var layoutDimension: NSCollectionLayoutDimension {
+            switch self {
+            case let .estimated(height):
+                return .estimated(height)
+            case let .absolute(height):
+                return .absolute(height)
+            case let .fractional(height):
+                return .fractionalHeight(height)
+            }
+        }
+    }
+
+    static func fullWidth(
+        groupHeight: Height,
+        itemHeight: Height = .fractional(1.0),
+        interGroupSpacing: CGFloat = Spacing.small,
+        contentInsets: NSDirectionalEdgeInsets = .init(top: Spacing.small,
+                                                       leading: Spacing.medium,
+                                                       bottom: Spacing.small,
+                                                       trailing: Spacing.medium)
+        )-> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: itemHeight.layoutDimension)
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: groupHeight.layoutDimension)
+
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize,
+                                                     subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = interGroupSpacing
+        section.contentInsets = contentInsets
+
+        return section
+    }
+}
